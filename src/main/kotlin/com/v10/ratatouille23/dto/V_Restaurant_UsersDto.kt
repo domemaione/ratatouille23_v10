6a96776eb29cc20a9cd931e35d7902029407d828
domenicomaione <domenico.maione@outlook.it>
2023-01-23 12:49:23 +0100

    [add] View V_Restaurant_Users and V_Restaurant_UsersDto

---
+++ src/main/kotlin/com/v10/ratatouille23/dto/V_Restaurant_UsersDto.kt
@@ -0,0 +1,14 @@
+package com.v10.ratatouille23.dto
+
+data class V_Restaurant_UsersDto(
+    var id: Long?,
+    var email: String?,
+    var name: String?,
+    var surname: String?,
+    var password: String?,
+    var role: String?,
+    var restaurantName: String?
+
+)
+
+
