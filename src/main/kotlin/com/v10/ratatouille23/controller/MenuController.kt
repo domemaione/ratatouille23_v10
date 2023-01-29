5850a4dba908ce116194ec3314423e2ea7ee8ad0
domenicomaione <domenico.maione@outlook.it>
2023-01-29 18:44:44 +0100

    [add] Constraint unique user_id in restaurant table (rel: one to one)

---
+++ src/main/kotlin/com/v10/ratatouille23/controller/MenuController.kt
@@ -0,0 +1,7 @@
+package com.v10.ratatouille23.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MenuController {
+}
--- src/main/kotlin/com/v10/ratatouille23/dto/MenuDto.kt
+++ src/main/kotlin/com/v10/ratatouille23/dto/MenuDto.kt
@@ -3,5 +3,4 @@ package com.v10.ratatouille23.dto
 data class MenuDto (
     var id: Long?,
     var restaurantId: Long
-    //var dishes: MutableList<DishDto>?
     )
