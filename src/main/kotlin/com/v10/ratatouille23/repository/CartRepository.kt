2b62016666d51ba37727437e4d29a41b5352c6a2
domenicomaione <domenico.maione@outlook.it>
2023-03-14 12:24:07 +0100

    [bug fix] add cart in CartService

--- src/main/kotlin/com/v10/ratatouille23/repository/CartRepository.kt
+++ src/main/kotlin/com/v10/ratatouille23/repository/CartRepository.kt
@@ -2,10 +2,13 @@ package com.v10.ratatouille23.repository
 
 
 import com.v10.ratatouille23.model.Cart
+import com.v10.ratatouille23.utils.CartStatus
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CartRepository : JpaRepository<Cart, Long> {
-    fun findByTableId(tableId: Long): Cart?
+    @Query("SELECT c FROM Cart c WHERE c.tableId = :tableId AND c.status = :status")
+    fun findByTableIdAndStatus(tableId: Long, status: CartStatus): Cart?
 }
--- src/main/kotlin/com/v10/ratatouille23/service/CartService.kt
+++ src/main/kotlin/com/v10/ratatouille23/service/CartService.kt
@@ -28,11 +28,9 @@ class CartService(
     fun add(tableId: Long, cartRequestDto: CartRequestDto): Cart {
         val user = AuthenticatedUserHelper.get() ?: throw IllegalStateException("User not valid")
         val tableRestaurantId = this.tableRestaurantRepository.getReferenceById(tableId).id ?: throw IllegalStateException("Table not found")
-        val found = this.cartRepository.findByTableId(tableId)
+        val found = this.cartRepository.findByTableIdAndStatus(tableId, CartStatus.OPEN)
 
-        //una get reference nella tabella cart_dish che se non trova un id del cameriere nell'ordine trovato allora si procede all'ordine
-        //TODO devo controllare se l'ordine esistente ed aperto non venga effettuato da un altro cameriere
-        if (found != null && found.status == CartStatus.OPEN) {
+        if (found != null) {
             if (cartRequestDto.dishes != null) {
                 val toSaveDishes = cartRequestDto.dishes.map {
                     CartDish(
@@ -47,7 +45,7 @@ class CartService(
                 this.cartDishRepository.saveAll(toSaveDishes)
             }
 
-        } else if ((found == null) || (found.status == CartStatus.CLOSED)) {
+        } else {
             val toSave = Cart(
                 id = null,
                 tableId = tableId,
@@ -100,3 +98,53 @@ class CartService(
 
 
 }
+
+
+
+
+/* @Transactional
+    fun add(tableId: Long, cartRequestDto: CartRequestDto): Cart {
+        val user = AuthenticatedUserHelper.get() ?: throw IllegalStateException("User not valid")
+        val tableRestaurantId = this.tableRestaurantRepository.getReferenceById(tableId).id ?: throw IllegalStateException("Table not found")
+        val found = this.cartRepository.findByTableId(tableId)
+        if (found != null && found.status == CartStatus.OPEN) {
+            if (cartRequestDto.dishes != null) {
+                val toSaveDishes = cartRequestDto.dishes.map {
+                    CartDish(
+                        id = null,
+                        cartId = found.id!!,
+                        it,
+                        userId = user.id!!,
+                        createdAt = LocalDateTime.now(),
+                        updateAt = LocalDateTime.now()
+                    )
+                }
+                this.cartDishRepository.saveAll(toSaveDishes)
+            }
+
+        } else if ((found == null) || (found.status == CartStatus.CLOSED)) {
+            val toSave = Cart(
+                id = null,
+                tableId = tableId,
+                status = CartStatus.OPEN,
+                createdAt = LocalDateTime.now(),
+                updateAt = null //TODO da controllare
+            )
+            val savedOrder = this.cartRepository.save(toSave)
+            if (cartRequestDto.dishes != null) {
+                val toSaveDishes = cartRequestDto.dishes.map {
+                    CartDish(
+                        id = null,
+                        cartId = savedOrder.id!!,
+                        it,
+                        userId = user.id!!,
+                        createdAt = LocalDateTime.now(),
+                        updateAt = LocalDateTime.now()
+                    )
+                }
+                this.cartDishRepository.saveAll(toSaveDishes)
+            }
+            return savedOrder
+        }
+        return found
+    }*/
